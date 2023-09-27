d950373073dd52f070ecae0baadded813bc47cf8
csgutierm <csgutier@gmail.com>
2023-09-27 00:16:00 -0300

    Contador

--- app/src/main/java/com/csgm/foregroundservice/MainActivity.kt
+++ app/src/main/java/com/csgm/foregroundservice/MainActivity.kt
@@ -8,7 +8,10 @@ import android.util.Log
 import com.csgm.foregroundservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), Handler.Callback{
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -20,9 +23,10 @@ class MainActivity : AppCompatActivity(), Handler.Callback{
                 binding.btStartStop.text=getString(R.string.messsage_start)
             }
             else {
-                ForegroundService.startService(this,getString(R.string.message_2_user),
-                    Handler(this))
+                ForegroundService.startService(this,getString(R.string.message_2_user), Handler(this))
                 binding.btStartStop.text=getString(R.string.messsage_stop)
+
+                startCounterTimer()
             }
 
         }
@@ -34,4 +38,27 @@ class MainActivity : AppCompatActivity(), Handler.Callback{
         binding.tvCounter.text=count.toString()
         return true
     }
+
+    private fun updateCounterView() {
+        binding.tvCounter.text = counter.toString()
+    }
+
+    private val counterHandler = Handler()
+    private val counterRunnable = object : Runnable {
+        override fun run() {
+            counter++
+            updateCounterView()
+            // Programar la próxima ejecución del temporizador en 7 segundos
+            counterHandler.postDelayed(this, 7000)
+        }
+    }
+
+    private fun startCounterTimer() {
+        counter = 0 // Reiniciar el contador
+        updateCounterView()
+        // Iniciar el temporizador para la primera ejecución
+        counterHandler.postDelayed(counterRunnable, 7000)
+    }
+
+
 }
